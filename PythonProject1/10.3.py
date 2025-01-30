52921d7f1f6fa3091c9fc9524ff52dd7bdaaf689
Tomas Bernotas <email@email.com>
2025-01-30 20:24:36 +0200

    10.4

---
+++ PythonProject1/10.3.py
@@ -0,0 +1,8 @@
+from math import sqrt as sq
+from math import *
+
+result = sq(625)
+print("Kvadratinė šaknis iš 625 yra:", result)
+
+result = sin(radians(90))
+print(f"Sinusas iš 90 laipsnių yra: {result}")
