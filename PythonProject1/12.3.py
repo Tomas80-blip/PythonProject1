b57158081ddb17734b2f68f3388d45aa1fb56ed9
Tomas Bernotas <email@email.com>
2025-01-30 20:49:26 +0200

    12.3

---
+++ PythonProject1/12.3.py
@@ -0,0 +1,20 @@
+# Sukurkite programą, kuri:
+# # 1. Prašo vartotojo įvesti skaičių.
+# # 2. Bandys konvertuoti į int.
+# # 3. Jei klaidos nėra, naudos else, kad atspausdintų "Konversija sėkminga:
+# # <skaičius>".
+# # 4. finally bloke atspausdins "Programa baigė darbą.", nepriklausomai nuo
+# # klaidų.
+
+
+
+try:
+        ivestis = input("Įveskite integer skaičių: ")
+        int_skaicius = int(ivestis)
+        print("Įvestis tinkama", int_skaicius)
+except ValueError:
+        print("Įvestis NETINKAMA, pakartokite!!!")
+else:
+        print(f'Konversija sėkminga: {int_skaicius}')
+finally:
+        print("Manęs niekaip neatsikratysit - FINALLY komanda")
