8710b41f00a2a5f4df808a5c635380b9266d4366
Tomas Bernotas <email@email.com>
2025-02-03 18:40:11 +0200

    13.1

---
+++ PythonProject1/13.1.py
@@ -0,0 +1,26 @@
+# 1. Atskiri datos-laiko laukai iš datetime objekto
+# Užduotis 1:
+# 1. Sukurkite programą, kuri:
+# a. Naudoja datetime.datetime.today() dabartinės datos ir laiko gavimui.
+# b. Išveda šiuos laukus: metai, mėnuo, diena, valanda, minutė, sekundė.
+# 2. Išveskite pranešimą:
+# "Dabar yra <valanda>:<minutė>, <diena>-<mėnuo>-<metai>"
+print('-------------------------------------------------')
+import datetime
+dt_res = datetime.datetime.today()
+metai = dt_res.year
+menuo = dt_res.month
+diena = dt_res.day
+valanda = dt_res.hour
+minute = dt_res.minute
+sekunde = dt_res.second
+
+print(dt_res)
+print(dt_res.year)  # 2025
+print(dt_res.month)
+print(dt_res.day)
+print(dt_res.hour)
+print(dt_res.minute)
+print(dt_res.second)
+
+print(f'Dabar yra {valanda}:{minute}, {diena}--{menuo}--{metai}
