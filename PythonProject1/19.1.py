8a36fd7033414edad74a5c406c18600df7f9d390
Tomas Bernotas <email@email.com>
2025-02-12 19:30:37 +0200

    19.1

---
+++ PythonProject1/19.1.py
@@ -0,0 +1,24 @@
+# 1. Prisijungimas prie Duomenų Bazės
+# Užduotis:
+# 1. Sukurkite Python programą, kuri:
+# a. Prisijungia prie SQLite duomenų bazės pavadinimu mokykla.db.
+# b. Sukuria lentelę mokykla su stulpeliais: pavadinimas (TEXT), adresas
+# (TEXT), mokiniu_skaicius (INTEGER).
+
+import sqlite3
+
+# Prisijungimas prie duomenų bazės
+conn = sqlite3.connect("mokykla.db")
+c = conn.cursor() # sukuriamas kursorius(tai atidarymas tam tikro veiksmo)
+
+# Lentelės sukūrimas
+c.execute(
+'''
+CREATE TABLE IF NOT EXISTS studentai (
+    pavadinimas TEXT,
+    adresas TEXT,
+    mokiniu_skaicius INTEGER)
+''')
+
+conn.commit() #commitinimas
+conn.close()
